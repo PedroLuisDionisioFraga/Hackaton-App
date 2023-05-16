c6edd4a6ce1147b2d534830f0d12e09c8e18289d
PedroLuisDionisioFraga <pedrodfraga@hotmail.com>
2023-05-16 07:59:33 -0300

    :hammer: Att o esqueleto da página

--- lib/Presentation/Pages/Authentication/sign_up.dart
+++ lib/Presentation/Pages/Authentication/sign_up.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/foundation.dart';
+import 'dart:js_interop';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Shared/Utils/task.dart';
+import 'package:flutter_application_1/Shared/Utils/validators_authentication.dart';
 import '../../../Database/firebase_helper.dart';
+import '../../../Shared/Widgets/app_bar.dart';
 import '../../../Shared/Widgets/text_form_field.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -11,28 +15,86 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  // Chave para validar formulário
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   // TextFormField base
-  final MyTextFormField basicInput = MyTextFormField.empty();
+  final MyTextFormField basicInput = const MyTextFormField.empty();
 
   // Controllers
   final TextEditingController username = TextEditingController();
   final TextEditingController email = TextEditingController();
   final TextEditingController password = TextEditingController();
+  final TextEditingController confirmPassword = TextEditingController();
   // FocusNames
   final FocusNode focusUsername = FocusNode();
   final FocusNode focusEmail = FocusNode();
   final FocusNode focusPassword = FocusNode();
+  final FocusNode focusConfirmPassword = FocusNode();
+
+  // Tasks de senha
+  List<Task> tasks = [
+    Task("Contém pelo menos um caractere especial", isCompleted: false),
+    Task("Contém pelo menos um número", isCompleted: false),
+    Task("Contém pelo menos uma letra maiúscula", isCompleted: false),
+    Task("Contém no mínimo 7 caracteres", isCompleted: false),
+  ];
+
+  bool isPasswordValid = true;
+
+  void checkTasks(String value) {
+    setState(() {
+      for (Task task in tasks) {
+        switch (task.description) {
+          case "Mínimo de 7 caracteres":
+            task.isCompleted = value.length >= 7;
+            break;
+          case "Pelo menos um caractere maiúsculo":
+            task.isCompleted = value.contains(RegExp(r"[A-Z]"));
+            break;
+          case "Pelo menos um número":
+            task.isCompleted = value.contains(RegExp(r"[0-9]"));
+            break;
+          case "Pelo menos um caractere especial":
+            task.isCompleted = value.contains(RegExp(r"[!@#$%/\^&*~(),.-?:{}|<=>]"));
+            break;
+        }
+      }
+    });
+  }
+
+  // Para mostrar ou não a senha
+  bool showPassword = true;
+  bool showConfirmPassword = true;
+  void changingVisibilityOfPassword(String whereShowPassword) {
+    setState(() {
+      if (whereShowPassword == "Password") {
+        showPassword = !showPassword;
+      } else {
+        showConfirmPassword = !showConfirmPassword;
+      }
+    });
+  }
 
   void onFieldSubmitted(String text) {
-    if (kDebugMode) {
-      print("Email: ${email.text} e senha ${password.text}");
-      print("Texto: ${text}");
-      FirebaseHelper.signUp(name: username.text, email: email.text, password: password.text);
+    if (password.text == confirmPassword.text) {
+      if (formKey.currentState!.validate()) {
+        formKey.currentState!.save();
+        FirebaseHelper.signUp(
+          name: username.text,
+          email: email.text,
+          password: password.text,
+        );
+      }
+    } else {
+      // Fazer um aviso aqui
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    // Lista de tasks
+    final Widget taskList = Task.tasks(tasks);
+    //* Username
     final Widget usernameField = basicInput
         .copyWith(
           controller: username,
@@ -42,10 +104,10 @@ class _SignUpPageState extends State<SignUpPage> {
           capitalization: TextCapitalization.sentences,
           label: "Usuário*",
           startIcon: Icons.person,
-          onFieldSubmitted: onFieldSubmitted,
+          validator: EntriesValidator.validateUsername,
         )
         .inputField();
-
+    //* Email
     final Widget emailField = basicInput
         .copyWith(
           controller: email,
@@ -54,34 +116,128 @@ class _SignUpPageState extends State<SignUpPage> {
           inputType: TextInputType.name,
           label: "Email*",
           startIcon: Icons.email,
+          validator: EntriesValidator.validateEmail,
         )
         .inputField();
-
+    //* Senha
     final Widget passwordField = basicInput
         .copyWith(
           controller: password,
           focusName: focusPassword,
-          inputAction: TextInputAction.done,
+          inputAction: TextInputAction.next,
           inputType: TextInputType.name,
           label: "Senha*",
           startIcon: Icons.lock,
+          validator: EntriesValidator.validatePassword,
+          obscureText: showPassword,
+          suffixIcon: IconButton(
+            color: isPasswordValid ? null : Theme.of(context).colorScheme.error,
+            icon: Icon(
+              showPassword ? Icons.visibility : Icons.visibility_off,
+            ),
+            onPressed: () {
+              changingVisibilityOfPassword("Password");
+            },
+          ),
+          onEditingComplete: () {
+            FocusScope.of(context).requestFocus(focusConfirmPassword);
+          },
+        )
+        .inputField();
+    //* Confirmar Senha
+    final Widget confirmPasswordField = basicInput
+        .copyWith(
+          controller: confirmPassword,
+          focusName: focusConfirmPassword,
+          inputAction: TextInputAction.done,
+          inputType: TextInputType.name,
+          label: "Confirmar Senha*",
+          startIcon: Icons.lock,
+          obscureText: showConfirmPassword,
+          validator: (confirmPassword) {
+            print(confirmPassword);
+            print(password.text);
+            return EntriesValidator.validateConfirmPassword(confirmPassword: confirmPassword, password: password.text);
+          },
+          suffixIcon: IconButton(
+            color: isPasswordValid ? null : Theme.of(context).colorScheme.error,
+            icon: Icon(
+              showConfirmPassword ? Icons.visibility : Icons.visibility_off,
+            ),
+            onPressed: () {
+              changingVisibilityOfPassword("Confirm Password");
+            },
+          ),
           onFieldSubmitted: onFieldSubmitted,
         )
         .inputField();
-        
+
     return Scaffold(
-      body: Center(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8.0),
-          children: [
-            const SizedBox(height: 30),
-            usernameField,
-            const SizedBox(height: 10),
-            emailField,
-            const SizedBox(height: 10),
-            passwordField,
-          ],
-        ),
+      body: Column(
+        children: [
+          const Align(
+            alignment: Alignment.topCenter,
+            //child: MainButtons(),
+          ),
+          Center(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: Card(
+                color: const Color.fromARGB(255, 255, 193, 7),
+                child: Form(
+                  key: formKey,
+                  child: ListView(
+                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 27.5),
+                    shrinkWrap: true,
+                    children: [
+                      const SizedBox(height: 20),
+                      Align(
+                        child: Text(
+                          "Cadastrar-se",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+                      usernameField,
+                      const SizedBox(height: 30),
+                      emailField,
+                      const SizedBox(height: 30),
+                      passwordField,
+                      const SizedBox(height: 30),
+                      taskList,
+                      const SizedBox(height: 30),
+                      confirmPasswordField,
+                      const SizedBox(height: 30),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 10, right: 15, top: 20),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              print(isPasswordValid);
+                              if (formKey.currentState!.validate()) {
+                                formKey.currentState!.save();
+                                setState(() {
+                                  isPasswordValid = true;
+                                });
+                                FirebaseHelper.signUp(name: username.text, email: email.text, password: password.text);
+                              } else {
+                                setState(() {
+                                  isPasswordValid = false;
+                                });
+                              }
+                            },
+                            child: const Text("Cadastro"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
